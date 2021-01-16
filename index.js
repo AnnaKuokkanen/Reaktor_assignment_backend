10c21210ceba729a0a7cb33034179e38916a2f87
Anna Kuokkanen <kuokkanenanna9@gmail.com>
2021-01-16 13:53:43 +0200

    Deploy to heroku

--- index.js
+++ index.js
@@ -26,7 +26,7 @@ app.get('/api/availability/:manufacturer', (req, res) => {
     })
 })
 
-const PORT = 3001
+const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
 })

d7a874480ba51a1b79acec8d7ef53e39467fec8e
Anna Kuokkanen <kuokkanenanna9@gmail.com>
2021-01-19 00:29:02 +0200

    Add an error middleware for unknown endpoint, generate a new production build

--- index.js
+++ index.js
@@ -23,6 +23,12 @@ app.get('/api/availability/:manufacturer', (req, res) => {
     })
 })
 
+const unknownEndpoint = (request, response) => {
+  response.status(404).send({ error: 'unknown endpoint' })
+}
+
+app.use(unknownEndpoint)
+
 const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
